5f645d0f4382f4f5f2ef5dabe530be100114dd57
MidTerm <83219310+MidTerm-CN@users.noreply.github.com>
2023-07-24 01:58:08 +0800

    Delete src directory

--- src/umpatcher/umpatcher/NRT.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace System.Runtime.CompilerServices {
-	[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
-	sealed class NotNullWhenTrueAttribute : Attribute {
-		public NotNullWhenTrueAttribute() { }
-	}
-}
